6020a1fedd49a98d2331a1b6e3895f1c8779c5f3
Toyosi Salami <omotoyosisalami00@gmail.com>
2022-03-18 02:06:09 +0100

    one more thing

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
           </div>
         </div>
 
-        {data.name != undefined && (
+        {data.name !== undefined && (
           <div className="bottom">
             <div className="feels">
               {data.main ? <p>{data.main.feels_like}°F</p> : null}
